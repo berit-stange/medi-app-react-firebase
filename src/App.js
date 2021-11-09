ff01fb13f23f6802b3a9a80dc779f5a9cc754cb9
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-11-09 18:29:06 +0100

    replaced user with medication + changed css

--- src/App.js
+++ src/App.js
@@ -11,19 +11,27 @@ import {
 } from "firebase/firestore";
 
 function App() {
-  const [newName, setNewName] = useState("");
-  const [newAge, setNewAge] = useState(0);
+  // const [firstName, setNewName] = useState("");
+  // const [lastName, setNewLastName] = useState("");
+  const [title, setNewTitle] = useState("");
+  const [body, setNewBody] = useState("");
 
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  // const [users, setUsers] = useState([]);
+  const [medication, setMedication] = useState([]);
+  // const usersCollectionRef = collection(db, "users");
+  const usersCollectionRef = collection(db, "medication");
 
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newName, age: Number(newAge) });
+  // const createUser = async () => {
+  //   await addDoc(usersCollectionRef, { firstName: firstName, lastName: lastName });
+  // };
+
+  const createMedi = async () => {
+    await addDoc(usersCollectionRef, { title: title, body: body });
   };
 
-  const updateUser = async (id, age) => {
+  const updateUser = async (id, lastName) => {
     const userDoc = doc(db, "users", id);
-    const newFields = { age: age + 1 };
+    const newFields = { lastName: lastName + 1 };
     await updateDoc(userDoc, newFields);
   };
 
@@ -32,57 +40,77 @@ function App() {
     await deleteDoc(userDoc);
   };
 
-  useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const data = await getDocs(usersCollectionRef);
+  //     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   };
+  //   getUsers();
+  // }, []);
 
-    getUsers();
+  useEffect(() => {
+    const getMedication = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setMedication(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getMedication();
   }, []);
 
-  return (
-    <div className="App">
-      <input
-        placeholder="Name..."
-        onChange={(event) => {
-          setNewName(event.target.value);
-        }}
-      />
-      <input
-        type="number"
-        placeholder="Age..."
-        onChange={(event) => {
-          setNewAge(event.target.value);
-        }}
-      />
 
-      <button onClick={createUser}> Create User</button>
-      {users.map((user) => {
+  return (
+    <div>
+
+      <div className="">
+        <input
+          placeholder="Medication..."
+          onChange={(event) => {
+            // setNewName(event.target.value);
+          }}
+        />
+        <input
+          // type="number"
+          placeholder="Take at..."
+          onChange={(event) => {
+            // setNewLastName(event.target.value);
+          }}
+        />
+        <button className="btn-add" onClick={createMedi}>+</button>
+      </div>
+
+
+      {medication.map((medication) => {
         return (
-          <div>
-            {" "}
-            <h1>Name: {user.name}</h1>
-            <h1>Age: {user.age}</h1>
-            <button
-              onClick={() => {
-                updateUser(user.id, user.age);
-              }}
-            >
-              {" "}
-              Increase Age
-            </button>
-            <button
-              onClick={() => {
-                deleteUser(user.id);
-              }}
-            >
-              {" "}
-              Delete User
-            </button>
+          <div className="container">
+            {/* {" "} */}
+
+            <p>Medication: {medication.title}</p>
+            {/* <p>Last Name: {user.lastName}</p> */}
+
+            <div>
+              <button
+                onClick={() => {
+                  // updateUser(user.id, user.lastName);
+                }}
+              >
+                {" "}
+                +
+              </button>
+
+              <button
+                onClick={() => {
+                  // deleteUser(user.id);
+                }}
+              >
+                {" "}
+                -
+              </button>
+            </div>
+
           </div>
         );
       })}
+
+
     </div>
   );
 }
